846a355e88f7c00119a450381c959a21980fa1a4
SimranJmal <simranjamal7@gmail.com>
2023-12-16 00:27:46 +0530

    use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,41 +1,10 @@
-const products = [
-    {
-        img: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        price: 500,
-
-    },
-    {
-        img: "images/products/intermediate-composite-basketball.jpg",
-        name: " Intermediate Size Basketball",
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        price: 300,
-
-    },
-    {
-        img: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        price: 600
-    }];
-
 let productsHTML = '';
 products.forEach((product) => {
  productsHTML += `
  <div class="product-container">
     <div class="product-image-container">
       <img class="product-image"
-        src="${product.img}">
+        src="${product.image}">
     </div>
 
     <div class="product-name limit-text-to-2-lines">
