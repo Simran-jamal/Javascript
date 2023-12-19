67a677134f96b8d1d8704c76e6862a9058c43b03
SimranJmal <simranjamal7@gmail.com>
2023-12-19 14:49:33 +0530

    Save cart in local Storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,21 @@
-export let cart = [{
+export let cart =JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+ cart =  [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
 },{
     productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
 }];
+
+
+}
+function savetoStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart))
+  }
+
+
 
 export function addToCart(productId) {
     let machingItem;
@@ -13,6 +24,7 @@ export function addToCart(productId) {
         machingItem = cartItem;
       }
     });
+    
   
   
     const quantitySelect = document.querySelector(`.js-quantity-selector-${productId}`);
@@ -30,7 +42,10 @@ export function addToCart(productId) {
       });
   
     }
+    savetoStorage();
   }
+
+
   export function removeFromCart(productId){
     let newCart = [];
 
@@ -39,6 +54,10 @@ export function addToCart(productId) {
         newCart.push(cartItem);
         
       }
-    });cart = newCart;
+    });
+    
+    cart = newCart;
+
+    savetoStorage();
 
   }
